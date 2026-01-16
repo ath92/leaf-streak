0d28da15b1dab15d94003ce6b23a8c2f1517f22a
ath92 <tomhutman+git@gmail.com>
2026-01-16 14:15:46 +0100

    path name

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import { render } from 'preact'
 import './index.css'
 import { App } from './app.tsx'
+import { Home } from './components/Home.tsx'
 import { Route, Switch } from 'wouter'
 
 render(
   <Switch>
+    <Route path="/">
+      <Home />
+    </Route>
     <Route path="/:streakId">
       {(params) => <App streakId={params.streakId} />}
-    </Route>
-    <Route path="/">
-      <App streakId="default" />
     </Route>
   </Switch>,
   document.getElementById('app')!
